623ad85f913bd7f17b0e59e4b8dadccdc5bf6121
sawirricardo <sawir.ricardo@gmail.com>
2022-01-08 16:05:57 +0700

    added rate limiting to contact submission

--- app/Http/Livewire/Front/Contact.php
+++ app/Http/Livewire/Front/Contact.php
@@ -4,10 +4,14 @@ namespace App\Http\Livewire\Front;
 
 use App\Models\ContactSubmission;
 use Artesaos\SEOTools\Facades\SEOTools;
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
 use Livewire\Component;
 
 class Contact extends Component
 {
+    use WithRateLimiting;
+
     public $data = [
         'name' => '',
         'email' => '',
@@ -43,6 +47,13 @@ class Contact extends Component
     public function create()
     {
         $this->validate();
+
+        try {
+            $this->rateLimit(3);
+        } catch (TooManyRequestsException $exception) {
+            $this->addError('data.email', "Slow down! Please wait another $exception->secondsUntilAvailable seconds to log in.");
+            return;
+        }
 
         ContactSubmission::create($this->data);
 
