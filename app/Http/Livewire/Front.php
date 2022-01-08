ba93364971e927ce42e1dd46d380e29115d4a8d6
sawirricardo <sawir.ricardo@gmail.com>
2022-01-08 11:36:13 +0700

    Added SEO description

--- app/Http/Livewire/Front.php
+++ app/Http/Livewire/Front.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Support\Collection;
 use Livewire\Component;
 
@@ -21,6 +22,8 @@ class Front extends Component
         $this->clients = \App\Models\Client::query()
             ->with(['mediaUpload.media'])
             ->get();
+
+        SEOTools::setDescription('Crafting applications, websites, digital designs with heart.');
     }
     public function render()
     {
